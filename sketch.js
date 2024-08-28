4d94a8413d94c03b02797e5f25200586f40f5b15
jean-012 <00001121536177sp@al.educacao.sp.gov.br>
2024-08-28 15:59:02 -0300

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,19 @@
+function setup() {
+  createCanvas(1000, 1000);
+  background("green");
+}
+
+function draw() {
+  
+  
+  stroke("green");
+  fill("red");
+ //  onsole.log(mouseIsPRES)
+  
+  if (mouseIsPressed){
+  rect(mouseX, mouseY,98,63);{
+    
+  }
+}
+}
+  
